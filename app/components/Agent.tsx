bdbfee03ffac66ad380d58577dfc2de3412594b6
YourName <your-email@example.com>
2025-07-18 18:58:19 +0100

    Interview generation ui

--- app/components/Agent.tsx
+++ app/components/Agent.tsx
@@ -10,6 +10,11 @@ enum CallStatus{
 const Agent = ({userName}: AgentProps) => {
     const callStatus=CallStatus.FINISHED;
     const isSpeaking=true;
+    const messages=[
+      'Whats your name?',
+      'My name is John Doe, nice to meet you!'
+    ];
+    const lastMessage=messages[messages.length -1];
 
     
   return (
@@ -22,21 +27,25 @@ const Agent = ({userName}: AgentProps) => {
           </div>
           <h3>Ai Interviewer</h3>
       </div>
-
+    
       <div className='card-border'>
-         <div className='card-content'>
-            
+         <div className='card-content'>         
                 <Image src='/user-avatar.png' alt='user avatar' width={540} height={540} className='rounded-full object-cover size-[120px]'/>
-                <h3>{userName}</h3>
-          
+                <h3>{userName}</h3>      
          </div>
       </div>
     </div>
-
+   {messages.length > 0 &&(
+     <div className='transcript-border'>
+        <div className='transcript'>
+          <p key={lastMessage} className={cn('transition-opacity duration-500 opacity-0','animate-fadeIn opacity-100')}>{lastMessage}</p>
+        </div>
+     </div>
+   )}
     <div className='w-full flex justify-center'>
        {callStatus !=='ACTIVE'?(
         <button className='relative btn-call'>
-            <span className={cn(`absolute animate-ping rounded-full opacity-75`, callStatus !== 'CONNECTIng' & 'hiden')}
+            <span className={cn(`absolute animate-ping rounded-full opacity-75`, callStatus !== 'CONNECTIng' & 'hidden')}
            />
            <span>
              {callStatus ==='INACTIVE' || callStatus=== 'FINISHED'? 'Call':'...' }
